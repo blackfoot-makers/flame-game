ba70bb19183872d90da21cfa7fcd369dde3176a1
Nirsu <melvyn.fontaine@epitech.eu>
2022-07-30 19:12:59 +0200

    [FEAT]: load map [WIP]: map in tiled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   final MelvynPlusPlusGame game = MelvynPlusPlusGame();
   runApp(GameWidget<MelvynPlusPlusGame>(game: game));
 }
@@ -9,7 +13,12 @@ void main() {
 class MelvynPlusPlusGame extends FlameGame {
   @override
   Future<void> onLoad() async {
-    // TODO(Mekvyn) Load base sprites here
+    await super.onLoad();
+    final TiledComponent tiledMap = await TiledComponent.load(
+      'map.tmx',
+      Vector2.all(16),
+    );
+    unawaited(add(tiledMap));
     // await Flame.images.load('klondike-sprites.png');
   }
 }
