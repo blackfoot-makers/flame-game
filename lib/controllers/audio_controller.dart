b03181800b5a052a0f92e0adbbc5369466ea92fe
tun43p <git@tun43p.com>
2022-07-30 19:38:37 +0200

    feat: rename audio files and update audio controller

--- lib/controllers/audio_controller.dart
+++ lib/controllers/audio_controller.dart
@@ -1,14 +1,18 @@
 import 'package:flame_audio/flame_audio.dart';
 
+class Audio {}
+
 class AudioController {
   Future<void> cacheAllAudioFiles() async {
     await FlameAudio.audioCache.loadAll(
       <String>[
         'ambiance.wav',
         'fire.wav',
-        'footsteps.wav',
-        'pick-up.wav',
-        'refill.wav'
+        'pick_up_1.wav',
+        'pick_up_2.wav',
+        'refill.wav',
+        'run.wav',
+        'walk.wav'
       ],
     );
   }
@@ -17,8 +21,16 @@ class AudioController {
     FlameAudio.bgm.initialize();
   }
 
+  void disposeBackgroundMusic() {
+    FlameAudio.bgm.dispose();
+  }
+
+  void stopBackgroundMusic() {
+    FlameAudio.bgm.stop();
+  }
+
   void playBackgroundMusic() {
-    FlameAudio.bgm.play('ambiance.wav');
+    FlameAudio.bgm.play('ambiance.wav', volume: 0.25);
   }
 
   void playFire() {
@@ -29,7 +41,17 @@ class AudioController {
     FlameAudio.play('refill.wav');
   }
 
-  void playPickUp() {
-    FlameAudio.play('pick-up.wav');
+  void playPickUp(int pickupIndex) {
+    FlameAudio.play('pick_up_$pickupIndex.wav');
+  }
+
+  void loopIsWalking() {
+    FlameAudio.play('walk.wav');
+  }
+
+  void loopIsRunning(bool isRunning) {
+    if (isRunning) {
+      FlameAudio.loop('run.wav');
+    }
   }
 }
