664b805b761d4cc89dc1e9d203c667a50a4199b1
Nirsu <melvyn.fontaine@epitech.eu>
2022-08-20 22:40:12 +0200

    [WIP]: add Forge2d -> rewrite the app with it [FEAT]: add an asset to player

--- lib/main_game/melvyn_plus_plus_game.dart
+++ lib/main_game/melvyn_plus_plus_game.dart
@@ -5,6 +5,7 @@ import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_game/audio/audio_constants.dart';
 import 'package:flame_game/audio/audio_controller.dart';
 import 'package:flame_game/main_game/constant.dart';
@@ -15,12 +16,10 @@ import 'package:flutter/rendering.dart';
 // ignore: depend_on_referenced_packages
 import 'package:tiled/tiled.dart';
 
-class MelvynPlusPlusGame extends FlameGame
-    with
-        PanDetector,
-        HasTappableComponents,
-        HasDraggables,
-        HasCollisionDetection {
+/// The [MelvynPlusPlusGame] is the main game class.
+///
+/// It is responsible for creating the game and initializing the components.
+class MelvynPlusPlusGame extends Forge2DGame {
   late Player player;
   late final JoystickComponent joystick;
 
@@ -28,16 +27,6 @@ class MelvynPlusPlusGame extends FlameGame
   Future<void> onLoad() async {
     await super.onLoad();
 
-    await AudioController.initialize();
-    await AudioController.playBackgroundMusic(kAudioAmbianceFile);
-
-    final TiledComponent tiledMap = await TiledComponent.load(
-      'map.tmx',
-      kTitleSize,
-    );
-    unawaited(add(tiledMap));
-
-    _loadWall(tiledMap);
     final Paint knobPaint = BasicPalette.white.withAlpha(200).paint();
     final Paint backgroundPaint = BasicPalette.white.withAlpha(100).paint();
     joystick = JoystickComponent(
@@ -46,25 +35,64 @@ class MelvynPlusPlusGame extends FlameGame
       margin: const EdgeInsets.only(left: 40, bottom: 40),
     );
 
-    player = Player(joystick);
+    player = Player(
+      position: Vector2(20, 20),
+      size: Vector2(4, 4),
+    );
     unawaited(add(player));
-
-    unawaited(add(joystick));
-  }
-
-  void _loadWall(TiledComponent tiledMap) {
-    final ObjectGroup? walls =
-        tiledMap.tileMap.getLayer<ObjectGroup>(kWallsLayer);
-
-    for (final TiledObject wall in walls!.objects) {
-      unawaited(
-        add(
-          Wall(
-            size: Vector2(wall.width, wall.height),
-            position: Vector2(wall.x, wall.y),
-          ),
-        ),
-      );
-    }
   }
 }
+
+// class MelvynPlusPlusGame extends FlameGame
+//     with
+//         PanDetector,
+//         HasTappableComponents,
+//         HasDraggables,
+//         HasCollisionDetection {
+//   late Player player;
+//   late final JoystickComponent joystick;
+
+//   @override
+//   Future<void> onLoad() async {
+//     await super.onLoad();
+
+//     await AudioController.initialize();
+//     await AudioController.playBackgroundMusic(kAudioAmbianceFile);
+
+//     final TiledComponent tiledMap = await TiledComponent.load(
+//       'map.tmx',
+//       kTitleSize,
+//     );
+//     unawaited(add(tiledMap));
+
+//     _loadWall(tiledMap);
+//     final Paint knobPaint = BasicPalette.white.withAlpha(200).paint();
+//     final Paint backgroundPaint = BasicPalette.white.withAlpha(100).paint();
+// joystick = JoystickComponent(
+//   knob: CircleComponent(radius: 20, paint: knobPaint),
+//   background: CircleComponent(radius: 50, paint: backgroundPaint),
+//   margin: const EdgeInsets.only(left: 40, bottom: 40),
+// );
+
+//     player = Player(joystick);
+//     unawaited(add(player));
+
+//     unawaited(add(joystick));
+//   }
+
+//   void _loadWall(TiledComponent tiledMap) {
+//     final ObjectGroup? walls =
+//         tiledMap.tileMap.getLayer<ObjectGroup>(kWallsLayer);
+
+//     for (final TiledObject wall in walls!.objects) {
+//       unawaited(
+//         add(
+//           Wall(
+//             size: Vector2(wall.width, wall.height),
+//             position: Vector2(wall.x, wall.y),
+//           ),
+//         ),
+//       );
+//     }
+//   }
+// }
--- lib/main_game/player.dart
+++ lib/main_game/player.dart
@@ -2,81 +2,174 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_game/audio/audio_controller.dart';
 import 'package:flame_game/main_game/constant.dart';
 import 'package:flame_game/main_game/melvyn_plus_plus_game.dart';
 import 'package:flame_game/wall/wall.dart';
 import 'package:flutter/material.dart';
 
-class Player extends PositionComponent
-    with HasGameRef<MelvynPlusPlusGame>, CollisionCallbacks {
-  Player(this.joystick);
+class Player extends BodyComponent<Forge2DGame> {
+  Player({
+    required this.position,
+    required this.size,
+  });
 
-  static final Paint _paint = Paint()..color = Colors.white;
   static const double _playerSpeed = 200;
-
-  final JoystickComponent joystick;
-  bool _isCollided = false;
-  JoystickDirection _lastDirection = JoystickDirection.idle;
-
-  bool _isAlreadyRunning = false;
-
-  void _playRunningAudio() {
-    if (!_isAlreadyRunning) {
-      AudioController.playerRunningAudioInstance.play();
-      _isAlreadyRunning = true;
-    }
-  }
-
-  void _stopRunningAudio() {
-    if (_isAlreadyRunning) {
-      AudioController.playerRunningAudioInstance.stop();
-      _isAlreadyRunning = false;
-    }
-  }
+  final Vector2 position;
+  final Vector2 size;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    position = gameRef.size / 2;
-    size = kTitleSize;
-    anchor = Anchor.center;
+    final Sprite sprite = await gameRef.loadSprite('tmp_player.png');
+    renderBody = false;
+    //TODO(smn): replace by a real sprite + add angle support
+    await add(
+      SpriteComponent()
+        ..sprite = await gameRef.loadSprite('tmp_player.png')
+        ..size = Vector2.all(6)
+        ..anchor = Anchor.center,
+    );
   }
 
   @override
-  void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
-  }
+  Body createBody() {
+    final PolygonShape shape = PolygonShape();
 
-  @override
-  void update(double dt) {
-    super.update(dt);
-    if (joystick.direction != JoystickDirection.idle) {
-      if (!_isCollided || _lastDirection != joystick.direction) {
-        position.add(joystick.relativeDelta * _playerSpeed * dt);
-      }
-      angle = joystick.delta.screenAngle();
-      _playRunningAudio();
-    } else {
-      _stopRunningAudio();
-    }
+    final List<Vector2> vertices = <Vector2>[
+      Vector2(-size.x / 2, size.y / 2),
+      Vector2(size.x / 2, size.y / 2),
+      Vector2(size.x / 2, -size.y / 2),
+      Vector2(-size.x / 2, -size.y / 2),
+    ];
+    shape.set(vertices);
 
-    gameRef.camera.followComponent(this);
-  }
+    final FixtureDef fixtureDef = FixtureDef(
+      shape,
+      userData: this, // To be able to determine object in collision
+    );
 
-  @override
-  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    super.onCollision(intersectionPoints, other);
-    if (other is Wall && !_isCollided) {
-      _isCollided = true;
-      _lastDirection = joystick.direction;
-    }
-  }
+    final BodyDef bodyDef = BodyDef(
+      position: position,
+      type: BodyType.dynamic,
+      active: false,
+    );
 
-  @override
-  void onCollisionEnd(PositionComponent other) {
-    super.onCollisionEnd(other);
-    _lastDirection = JoystickDirection.idle;
-    _isCollided = false;
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
+
+// class Player extends PositionComponent
+//     with HasGameRef<MelvynPlusPlusGame>, Hitbox, Collidable {
+//   Player(this.joystick);
+
+//   static final Paint _paint = Paint()..color = Colors.white;
+//   static const double _playerSpeed = 200;
+
+//   final JoystickComponent joystick;
+//   bool _isCollided = false;
+//   JoystickDirection _lastDirection = JoystickDirection.idle;
+
+//   bool _isAlreadyRunning = false;
+
+//   void _playRunningAudio() {
+//     if (!_isAlreadyRunning) {
+//       AudioController.playerRunningAudioInstance.play();
+//       _isAlreadyRunning = true;
+//     }
+//   }
+
+//   void _stopRunningAudio() {
+//     if (_isAlreadyRunning) {
+//       AudioController.playerRunningAudioInstance.stop();
+//       _isAlreadyRunning = false;
+//     }
+//   }
+
+  // @override
+  // Future<void> onLoad() async {
+  //   await super.onLoad();
+  //   position = gameRef.size / 2;
+  //   size = kTitleSize;
+  //   anchor = Anchor.center;
+  //   unawaited(add(RectangleHitbox()));
+  // }
+
+//   @override
+//   void render(Canvas canvas) {
+//     canvas.drawRect(size.toRect(), _paint);
+//   }
+
+//   void mooveInRightDirection(JoystickDirection direction, double dt) {
+//     print('lastDirection: $_lastDirection');
+//     print('isCollided: $_isCollided');
+//     if (_lastDirection == JoystickDirection.up &&
+//         direction == JoystickDirection.upRight) {
+//       position.x += joystick.relativeDelta.x * _playerSpeed * dt;
+//     }
+
+//     if (_lastDirection == JoystickDirection.up &&
+//         direction == JoystickDirection.upLeft) {
+//       position.x += joystick.relativeDelta.x * _playerSpeed * dt;
+//     }
+
+//     if (_lastDirection == JoystickDirection.left &&
+//         direction == JoystickDirection.upLeft) {
+//       position.y += joystick.relativeDelta.y * _playerSpeed * dt;
+//     }
+
+//     if (_lastDirection == JoystickDirection.left &&
+//         direction == JoystickDirection.downLeft) {
+//       position.y += joystick.relativeDelta.y * _playerSpeed * dt;
+//     }
+//     //   // String? _splitDirection;
+//     //   // if (direction == JoystickDirection.upLeft ||
+//     //   //     direction == JoystickDirection.upRight) {
+//     //   //   _splitDirection = direction.name.split('up').last;
+//     //   // }
+//     //   // print(_splitDirection);
+//     //   final String lowerDirection = direction.name.toLowerCase();
+//     //   final String lowerLastDirection = _lastDirection.name.toLowerCase();
+
+//     //   return lowerDirection.contains(lowerLastDirection);
+//   }
+
+//   @override
+//   void update(double dt) {
+//     super.update(dt);
+//     if (joystick.direction != JoystickDirection.idle) {
+//       // print(x);
+//       print(joystick.relativeDelta.y * _playerSpeed * dt);
+//       if (!_isCollided) {
+//         position.add(joystick.relativeDelta * _playerSpeed * dt);
+//       } else {
+//         mooveInRightDirection(joystick.direction, dt);
+//       }
+//       angle = joystick.delta.screenAngle();
+//       _playRunningAudio();
+//     } else {
+//       _stopRunningAudio();
+//     }
+
+//     gameRef.camera.followComponent(this);
+//   }
+
+//   @override
+//   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+//     super.onCollision(intersectionPoints, other);
+//     if (other is Wall && !_isCollided) {
+//       _isCollided = true;
+//       _lastDirection = joystick.direction;
+//     }
+//   }
+  
+//   @override
+//   void onCollisionEnd(PositionComponent other) {
+//     super.onCollisionEnd(other);
+//     if (_isCollided) {
+//       _lastDirection = JoystickDirection.idle;
+//       _isCollided = false;
+//     }
+//   }
+// }
