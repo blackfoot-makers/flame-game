2fd7171011ea1d615289fe250c1b5c7127c6d4ca
Nirsu <melvyn.fontaine@epitech.eu>
2022-08-28 20:33:11 +0200

    [FEAT]: set correct position for walls

--- lib/wall/wall.dart
+++ lib/wall/wall.dart
@@ -15,17 +15,13 @@ class Wall extends BodyComponent<Forge2DGame> with CollisionCallbacks {
   @override
   Body createBody() {
     // We don't render the body, because it's already on the map
-    // renderBody = false;
+    renderBody = false;
     final PolygonShape shape = PolygonShape();
 
-    debugPrint('wall Position: ${position.x}, ${position.y}');
-    debugPrint(
-      'wall Position / kTitleSize : ${position.x / kSpriteSize}, ${position.y / kSpriteSize}',
-    );
-    debugPrint('-----------------------------');
     shape.setAsBox(
       size.x / 2,
       size.y / 2,
+      // Set center of the sprite
       Vector2(0, 0),
       0,
     );
@@ -35,8 +31,18 @@ class Wall extends BodyComponent<Forge2DGame> with CollisionCallbacks {
       userData: this, // To be able to determine object in collision
     );
 
+    // We set the center of the sprites to 0,0.
+    // So we have to calculate by how many squares we have to move the sprite.
+    final Vector2 positionCorrection = Vector2(
+      size.x / kSpriteSize / 2,
+      size.y / kSpriteSize / 2,
+    );
+
     final BodyDef bodyDef = BodyDef(
-      position: Vector2(position.x + kSpriteSize, position.y + kSpriteSize),
+      position: Vector2(
+        position.x + (kSpriteSize * positionCorrection.x),
+        position.y + (kSpriteSize * positionCorrection.y),
+      ),
       userData: this,
       fixedRotation: true,
     );
